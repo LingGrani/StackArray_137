8a9561c170eeb9f355e7d4a63d11cce4305c4283
LingGrani <luthfiangger2510@gmail.com>
2023-06-15 15:45:55 +0700

    fungsi push element

--- StackArray/StackArray.cpp
+++ StackArray/StackArray.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 class StackArray {
-private:
+	private:
 	string stack_array[5];
 	int top;
 public:
@@ -11,5 +11,15 @@ public:
 	StackArray() {
 		top = -1;
 	}
-	
+	string push(string element) {
+		if (top == 4) { //step 1
+			cout << "Number of data exceeds the limit." << endl;
+			return "";
+		}
+		top++; //step 2
+		stack_array[top] = element; //step 3
+		cout << endl;
+		cout << element << " Ditambahkan(pushed)" << endl;
+
+	}
 };
