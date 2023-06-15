96e757f54c721f9967c8d630d927f5808088868a
LingGrani <luthfiangger2510@gmail.com>
2023-06-15 15:46:23 +0700

    return element

--- StackArray/StackArray.cpp
+++ StackArray/StackArray.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 class StackArray {
-	private:
+private:
 	string stack_array[5];
 	int top;
 public:
@@ -20,6 +20,6 @@ public:
 		stack_array[top] = element; //step 3
 		cout << endl;
 		cout << element << " Ditambahkan(pushed)" << endl;
-
+		return element;
 	}
 };
