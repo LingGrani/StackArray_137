8109cd2ab6d49a9c24a96dfcea002d6c2a05ff5b
LingGrani <luthfiangger2510@gmail.com>
2023-06-15 15:52:13 +0700

    While try catch copy-an

--- StackArray/StackArray.cpp
+++ StackArray/StackArray.cpp
@@ -40,9 +40,52 @@ public:
 			cout << "\nStack is empty." << endl;
 		}
 		else {
-			for (int top = 0; top < 5; top++) {
+			for (int top = 0; top <= 5; top++) {
 				cout << stack_array[top] << endl;
 			}
 		}
 	}
 };
+
+int main() {
+	while (true){
+		try {
+			cout << "\nMenu" << endl;
+			cout << "\1. Add a record to the list" << endl;
+			cout << "2. Delete a record from the list" << endl;
+			cout << "3. View all record in the ascending order of roll numbers" << endl;
+			cout << "4. View  all record in the descending order of roll number" << endl;
+			cout << "5. Search for a record in the list" << endl;
+			cout << "6. Exit" << endl;
+			cout << "\nEnter your Choise (1-6): ";
+			char ch;
+			cin >> ch;
+
+			switch (ch){
+			case'1':
+				obj.addNode();
+				break;
+			case'2':
+				obj.hapus();
+				break;
+			case'3':
+				obj.traverse();
+				break;
+			case'4':
+				obj.revtraverse();
+				break;
+			case'5':
+				obj.searchdata();
+				break;
+			case'6':
+				return 0;
+			default:
+				cout << "\nInvalid Option" << endl;
+				break;
+			}
+		}
+		catch (exception& e) {
+			cout << "Check for the values entered." << endl;
+		}
+	}
+}
